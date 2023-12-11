5b85432af181b4886f16d6eba6d03d193fef344d
moonglow <maxfunmaxfun672@gmail.com>
2023-12-12 00:11:17 +0300

    use of Sergey's function

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "input_matrix.h"
 #include "display_matrix.h"
+#include "multiplication_matrix_by_number.h"
 using namespace std;
 
 int main() {
@@ -9,8 +10,14 @@ int main() {
 	std::vector<std::vector<double>> myMatrix = inputMatrix();
 	setlocale(LC_ALL, "ru");
 	if (!myMatrix.empty()) {
-		cout << "Введенная матрица:" << endl;
-		displayMatrix(myMatrix);
+			cout << "Введенная матрица:" << endl;
+			displayMatrix(myMatrix);
+
+			myMatrix =multiplyMatrixByNumber(myMatrix);
+			cout << "Умноженная матрица:" << endl;
+			 displayMatrix(myMatrix);
+
 	}
+
 	return 0;
 }
