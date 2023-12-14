91b3feaeacbbd88b92e98d1a4225eb8fcb7825eb
”KuchN” <kucheruk05work@gmail.com>
2023-12-14 16:52:33 +0300

    added matrixes production modules and header for it

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -3,21 +3,50 @@
 #include "input_matrix.h"
 #include "display_matrix.h"
 #include "multiplication_matrix_by_number.h"
+#include "matrix_by_matrix_product.h"
 using namespace std;
 
 int main() {
-
-	std::vector<std::vector<double>> myMatrix = inputMatrix();
 	setlocale(LC_ALL, "ru");
-	if (!myMatrix.empty()) {
-			cout << "Введенная матрица:" << endl;
-			displayMatrix(myMatrix);
+	int mode;
+	cout << "Выберите режим работы программы:" << endl << "1-умножение матрицы на число" << endl << "2-умножение матриц" << endl;
+	cin >> mode;
 
-			myMatrix =multiplyMatrixByNumber(myMatrix);
+
+	switch(mode) {
+	case 1:{
+			std::vector<std::vector<double>> myMatrix = inputMatrix();
+			if (!myMatrix.empty()) {
+				cout << "Введенная матрица:" << endl;
+				displayMatrix(myMatrix);
+
+				myMatrix = multiplyMatrixByNumber(myMatrix);
+				cout << "Умноженная матрица:" << endl;
+				displayMatrix(myMatrix);
+
+			}
+		}
+	case 2: {
+		cin.ignore();
+
+		std::vector<std::vector<double>> Matrix_A = inputMatrix();
+		cout << "Введенная матрица:" << endl;
+		displayMatrix(Matrix_A);
+
+		std::vector<std::vector<double>> Matrix_B = inputMatrix();
+		cout << "Введенная матрица:" << endl;
+		displayMatrix(Matrix_B);
+		
+		if (!Matrix_A.empty() and !Matrix_B.empty()) {
+
+			std::vector<std::vector<double>> Matrix_res = multiplyMatrixByMatrix(Matrix_A, Matrix_B);
 			cout << "Умноженная матрица:" << endl;
-			 displayMatrix(myMatrix);
+			displayMatrix(Matrix_res);
 
+		}
+	}
 	}
 
 	return 0;
 }
+//
