860d03d5019e1cafb732d3834546c8c1ad7762a3
Alina Alshevskaya <alina.alshevskaya@bk.ru>
2023-12-18 20:24:17 +0300

    matrix.cpp added

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -3,6 +3,7 @@
 #include "input_matrix.h"
 #include "display_matrix.h"
 #include "multiplication_matrix_by_number.h"
+#include "determinant.h"
 using namespace std;
 
 int main() {
@@ -10,12 +11,15 @@ int main() {
 	std::vector<std::vector<double>> myMatrix = inputMatrix();
 	setlocale(LC_ALL, "ru");
 	if (!myMatrix.empty()) {
-			cout << "Введенная матрица:" << endl;
-			displayMatrix(myMatrix);
+		cout << "Введенная матрица:" << endl;
+		displayMatrix(myMatrix);
 
-			myMatrix =multiplyMatrixByNumber(myMatrix);
-			cout << "Умноженная матрица:" << endl;
-			 displayMatrix(myMatrix);
+		myMatrix = multiplyMatrixByNumber(myMatrix);
+		cout << "Умноженная матрица:" << endl;
+		displayMatrix(myMatrix);
+		
+		double det = determinant(myMatrix);
+		cout << "determinant: " << det;
 
 	}
 
